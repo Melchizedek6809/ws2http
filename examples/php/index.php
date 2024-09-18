6e6729477c084239e72ec4a43a622957f4fda316
Ben (MBA M2) <ben@wolkenwelten.net>
2024-09-19 01:29:40 +0900

    Initial experiment

---
+++ examples/php/index.php
@@ -0,0 +1,40 @@
+<?php
+$roomname = $_GET['room'] ?? "";
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>TinyChat <?= htmlspecialchars($roomname) ?></title>
+	<link rel="stylesheet" href="style.css" />
+	<script type="text/javascript" src="script.js" defer></script>
+</head>
+<body>
+	<main><?php
+if($roomname)  {
+	?>
+	<h1>TinyChat - <?= htmlspecialchars($roomname) ?></h1>
+	<div class="chat-wrap" room-name="<?= htmlspecialchars($roomname) ?>">
+		<div class="chat-users"></div>
+		<div class="chat-messages"></div>
+		<form>
+			<input type="text" name="msg" placeholder="Message" autocomplete="off"/>
+			<input type="submit" value="Send" />
+		</form>
+	</div>
+	<?php
+} else {
+	?>
+	<h1>TinyChat</h1>
+	<p>This is an example chat, built with WS2HTTP and PHP.</p>
+	<p>Please enter the name of the room you wish to join:</p>
+	<form action="/" method="GET">
+		<input type="text" name="room" placeholder="Room" required />
+		<input type="submit" value="Join" />
+	</form>
+	<?php
+}
+?>
+	</main>
+</body>
+</html>
---
+++ examples/php/script.js
@@ -0,0 +1,39 @@
+const wsUrl = 'ws://localhost:8080/ws';
+
+const initChat = chatWrap => {
+	const form = chatWrap.querySelector('form');
+	const users = chatWrap.querySelector('.chat-users');
+	const messages = chatWrap.querySelector('.chat-messages');
+	const input = form.querySelector('input[name="msg"]');
+
+	const ws = new WebSocket(wsUrl + `?room=${encodeURIComponent(chatWrap.dataset.room)}`);
+	ws.addEventListener('message', msg => {
+		showMessage(msg.data);
+	});
+
+	const showMessage = msg => {
+		const message = document.createElement('div');
+		message.classList.add('chat-message');
+		message.textContent = msg;
+		messages.appendChild(message);
+	};
+
+	const sendMessage = msg => {
+		ws.send(msg);
+	};
+
+	form.addEventListener('submit', e => {
+		e.preventDefault();
+		const msg = input.value;
+		if (!msg) return;
+		input.value = '';
+		sendMessage(msg);
+	});
+	input.focus();
+};
+
+const initChats = () => {
+	document.querySelectorAll('.chat-wrap').forEach(initChat);
+};
+
+setTimeout(initChats,0);
