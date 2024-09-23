9f3ab94c8b637bc4a843c115a550b6b82bfe5d1c
Ben (MBA M2) <ben@wolkenwelten.net>
2024-09-23 13:25:20 +0900

    More resilient server

--- examples/php/ws.php
+++ examples/php/ws.php
@@ -1,4 +1,30 @@
 <?php
 
-print_r($_COOKIE);
-print_r($_POST);
+$id = $_POST['id'] ?? "";
+$verb = $_POST['verb'] ?? "";
+$uri = $_POST['uri'] ?? "";
+$data = $_POST['uri'] ?? "";
+
+switch($verb){
+	case "OPEN":
+		if (!$id || !$uri) {
+			http_response_code(400);
+			die("Missing parameters");
+		}
+		break;
+	case "MESSAGE":
+		if (!$id || !$uri || !$data) {
+			http_response_code(400);
+			die("Missing parameters");
+		}
+		break;
+	case "CLOSE":
+	case "ERROR":
+		if (!$id || !$uri) {
+			http_response_code(400);
+			die("Missing parameters");
+		}
+		break;
+	default:
+		break;
+}
--- src/index.ts
+++ src/index.ts
@@ -3,51 +3,51 @@ import http from "node:http";
 import { WebSocketServer, WebSocket } from "ws";
 import express from "express";
 
-let idCounter = 0;
 let server: http.Server | undefined;
 const app = express();
-const sockets = new Set<WSConnection>();
+const sockets = new Map<string, WSConnection>();
 
 interface WSConnection {
-	id: number;
+	id: string;
 	uri: string;
 	cookies: string;
 	ws: WebSocket;
 }
 
 interface WSEvent {
-	verb: "OPEN" | "CLOSE" | "MESSAGE";
+	verb: "OPEN" | "MESSAGE" | "CLOSE" | "ERROR";
 	data?: unknown;
 }
 
-const dispatchEvent = async (con: WSConnection, event: WSEvent) => {
-	const formData = new FormData();
-	formData.set("id", String(con.id));
-	formData.set("uri", con.uri);
-	formData.set("verb", event.verb);
-	if (event.data) {
-		formData.set("data", event.data);
-	}
-
-	const res = await fetch("http://localhost:8000/ws.php", {
-		method: "POST",
-		headers: {
-			"cookie": con.cookies,
-		},
-		body: formData,
-	});
-	const text = await res.text();
-	console.log(text);
-};
-
-const receivedMessage = async (con: WSConnection, uri: string, msg: any) => {
-	for (const c of sockets.values()) {
-		if (c.ws === con.ws) {
-			continue;
-		}
-		c.ws.send(msg.toString('utf8'));
-	}
+// ToDo: a more sophisticated way to generate unique ids
+let idCounter = 0;
+const genSym = (): string => {
+	return String(++idCounter);
 }
+
+const dispatchEvent = async (con: WSConnection, event: WSEvent) => {
+	try {
+		const formData = new FormData();
+		formData.set("id", String(con.id));
+		formData.set("uri", con.uri);
+		formData.set("verb", event.verb);
+		if (event.data) {
+			formData.set("data", event.data);
+		}
+
+		const res = await fetch("http://localhost:8000/ws.php", {
+			method: "POST",
+			headers: {
+				cookie: con.cookies,
+			},
+			body: formData,
+		});
+		const text = await res.text();
+		console.log(text);
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 const serverListen = async () => {
 	app.use(async (req, res) => {
@@ -62,19 +62,30 @@ const serverListen = async () => {
 
 	const wss = new WebSocketServer({ server: server, path: "/ws" });
 	wss.on("connection", async (ws, req) => {
+		const id = genSym();
 		const cookies = req.headers.cookie || "";
 		const uri = req.url || "/ws";
-		const con = { id: ++idCounter, uri, cookies, ws };
-		sockets.add(con);
-		dispatchEvent(con, { verb: "OPEN" });
-		ws.on("message", data => {
-			dispatchEvent(con, { verb: "MESSAGE", data });
-			receivedMessage(con, uri, data);
-		});
-		ws.on("close", () => {
-			sockets.delete(con);
-			dispatchEvent(con, { verb: "CLOSE" });
-		});
+		const con = { id, uri, cookies, ws };
+		try {
+			sockets.set(id, con);
+
+			dispatchEvent(con, { verb: "OPEN" });
+
+			ws.on("message", (data) => dispatchEvent(con, { verb: "MESSAGE", data }));
+
+			ws.on("close", () => {
+				dispatchEvent(con, { verb: "CLOSE" });
+				sockets.delete(id);
+			});
+			ws.on("error", () => {
+				dispatchEvent(con, { verb: "ERROR" });
+				sockets.delete(id);
+			});
+		} catch (error) {
+			dispatchEvent(con, { verb: "ERROR" });
+			sockets.delete(id);
+			console.error(error);
+		}
 	});
 
 	return new Promise<void>((resolve) => {
