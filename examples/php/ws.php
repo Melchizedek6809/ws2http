83d98cd43c38fdb622b865e341bc9143164bd239
Ben (MBA M2) <ben@wolkenwelten.net>
2024-09-23 13:46:21 +0900

    Improved WS CGI handler

--- examples/php/ws.php
+++ examples/php/ws.php
@@ -1,11 +1,27 @@
 <?php
+$ws2http_url = "http://localhost:8080";
 
 function ws2http(string $uri, array $data) {
-	$ch = curl_init($uri);
+	$ch = curl_init($ws2http_url . $uri);
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 	$response = curl_exec($ch);
 	curl_close($ch);
+}
+
+function sendMessageToId(string $id, string $data) {
+	ws2http("", [
+		"verb" => "MESSAGE",
+		"id" => $id,
+		"data" => $data,
+	]);
+}
+
+function sendMessageToUri(string $uri, string $data) {
+	ws2http($uri, [
+		"verb" => "MESSAGE",
+		"data" => $data,
+	]);
 }
 
 $id   = $_POST['id']   ?? "";
